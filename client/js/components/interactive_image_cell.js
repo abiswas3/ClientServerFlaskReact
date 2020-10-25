a6bc1c83b48fea793a9c9366936571f84e36c9fd
Ari Biswas <aritrb@amazon.com>
2020-10-25 09:33:18 -0700

    Stacking almost works

--- client/js/components/interactive_image_cell.js
+++ client/js/components/interactive_image_cell.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 let IImgCell = ({imgToShow, feedback, flagOn, stackOn}) => {
 
-    let front = (imgToShow) => (<div >
+    let display = (imgToShow) => (<div >
                  <img src={imgToShow} className="image" ></img>           
                  <div className="middle">
                  <div className="btn-group">
@@ -13,29 +13,27 @@ let IImgCell = ({imgToShow, feedback, flagOn, stackOn}) => {
                  </div>                                                  
                  </div>);
 
-    // let back = (<div>
-    //              <img src={} className="image" ></img>           
-    //              <div className="middle">
-    //              <div className="btn-group">
-    //              <button className="btn btn-primary" onClick={()=>feedback(1, 0)}><i className="em em---1"></i></button>
-    //              <button className="btn btn-primary" onClick={()=>feedback(-1, 0)}><i className="em em--1"></i></button>
-    //              </div>
-    //              </div>                                                  
-    //             </div>);
+    let front = (<div className={"stacked"}>                
+                        <span className="blue">{display(imgToShow)}</span>
+                        </div>);
+
+    let back = (<div className={"stacked"}>                
+                <span className="blue">{display("https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/04/27/15563815885516.jpg")}</span>
+                </div>);
 
     console.log("Flag "+ flagOn);
 
     let front_stacked = (<div className={"stacked"}>                
                 <span className="red">Red</span>
                 <span className="green">Red</span>
-                <span className="blue">{front(imgToShow)}</span>
+                <span className="blue">{display(imgToShow)}</span>
                 </div>);
 
 
     let back_stacked = (<div className={"stacked"}>                
             <span className="red">Red</span>
             <span className="green">Red</span>
-            <span className="blue">{front("https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/04/27/15563815885516.jpg")}</span>
+            <span className="blue">{display("https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/04/27/15563815885516.jpg")}</span>
             </div>);
 
     if(stackOn)
@@ -48,8 +46,8 @@ let IImgCell = ({imgToShow, feedback, flagOn, stackOn}) => {
     else
         return (<div className={"scene"}>
                 <div className={"card" + (flagOn ? " is-flipped":"")}>
-                <div className="card__face card__face--front">{front(imgToShow)}</div>
-                <div className="card__face card__face--back">{front("https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/04/27/15563815885516.jpg")}</div>
+                <div className="card__face card__face--front">{front}</div>
+                <div className="card__face card__face--back">{back}</div>
                 </div>
                 </div>);
 }
